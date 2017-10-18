5af146233841f8eba5bd9779d596f0d56704209a
kazuhito-m <kazuhito.sumpic@gmail.com>
2017-10-19 06:37:24 +0900

    プロビジョニングファイル系変更。どうやら「生値」をコミットしていた…ようなので、ダミーに修正。 ま、一時的なもの＆Localのファイルなので問題無いが。

--- env/ci/sample.sh
+++ env/ci/sample.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Anasibleは「クライアント側にインストールされていること」を前提にします。
-ansible-playbook --private-key=~/Dropbox/keys/AWS/key/first_key.pem -i hosts -u ec2-user main.yml
+ansible-playbook --private-key=xxx.pem -i hosts -u root main.yml
